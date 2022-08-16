653c2a1b1f34883cc302f20763ffcb2a35116879
RiazTurhan <turhanuddin19711@gmail.com>
2022-08-16 23:22:06 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+ function dropdown(){
+    drop = document.querySelector("#dropdown").style.display = "block";
+    return drop
+}
+ function upDropDown(){
+    upDrop = document.querySelector("#dropdown").style.display = "none";
+    return upDrop;
+}
+
+function navber(){
+    navshow = document.querySelector("#navber").style.display = "inline-block"
+    return navshow;
+}
+
+function closeBar(){
+    barClose = document.querySelector('#navber').style.display = 'none'
+    return barClose;
+}
+
+
